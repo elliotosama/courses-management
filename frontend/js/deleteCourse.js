8e9734678eaed11631206c3a207f9e840bf1abb5
osama <osama@osama.osama>
2025-05-31 01:48:10 +0300

    adding update and delete courses

--- frontend/js/deleteCourse.js
+++ frontend/js/deleteCourse.js
@@ -1,1 +1,26 @@
-// file to handle delete course
+let container = document.querySelector('.container');
+let courseId = +window.location.search.split('=')[1];
+
+const data = {
+  id: courseId
+}
+
+const options = {
+  method: 'POST',
+  body: JSON.stringify(data)
+}
+
+if(confirm('are you sure?')) {
+  fetch('http://localhost/courseProject/api/deleteCourse.php', options)
+  .then(response => response.json())
+  .then(response => {
+    if(response['status'] === 'success') {
+      let alert = document.createElement('div');
+      alert.innerHTML = 'the course has been deleted';
+      alert.className = 'alert alert-success';
+      container.appendChild(alert)
+    }
+  })
+} else {
+  window.location = 'http://localhost/courseProject/frontend/index.html'
+}
--- frontend/js/updateCourse.js
+++ frontend/js/updateCourse.js
@@ -1,1 +1,77 @@
-// this file to handle the logic of updating course
+let courseId = +window.location.search.split('=')[1];
+const data = {
+  id: courseId
+}
+
+const options = {
+  method: 'POST',
+  body: JSON.stringify(data)
+}
+
+let form = document.querySelector('form');
+let courseName = document.getElementById('course')
+let startDate = document.getElementById('start_date')
+let endDate = document.getElementById('end_date')
+let loc = document.getElementById('location')
+let courseTime = document.getElementById('time')
+let firstDay = document.getElementById("day1")
+let secondDay = document.getElementById('day2')
+let done = document.getElementById('done')
+let sessionTaken = document.getElementById('sessionTaken')
+fetch('http://localhost/courseProject/api/readCourse.php', options)
+.then(response => response.json())
+.then(response => {
+  if(response['status'] === 'course not found'){
+    window.location = 'http://localhost/courseProject/frontend/index.html'
+  } else {
+    let course = response['course']
+    courseName.value = course['name'];
+    startDate.value = course['start_date'];
+    endDate.value = course['end_date'];
+    loc.value = course['location'];
+    courseTime.value = course['time'];
+    firstDay.value = course['first_day'];
+    secondDay.value = course['second_day'];
+    sessionTaken.value = course['session_taken']
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      let checked = 0;
+      if(done.checked) {
+        checked = 1;
+      }
+
+      const dataSend = {
+        id: courseId,
+        course_name: courseName.value,
+        start_date: startDate.value,
+        end_date: endDate.value,
+        location: loc.value,
+        time: courseTime.value,
+        done: checked,
+        session_taken: sessionTaken.value
+      }
+      const optionsTwo = {
+        method: 'POST',
+        body: JSON.stringify(dataSend)
+      }
+      console.log(optionsTwo)
+      fetch('http://localhost/courseProject/api/updateCourse.php', optionsTwo)
+        .then(one => one.json())
+        .then(one => {
+          if(one['status'] != 'success') {
+            alert('something went wrong')
+          } else {
+            window.location = 'http://localhost/courseProject/frontend/index.html';
+          }
+        })
+    })
+  } 
+})
+
+
+
+
+
+
+
